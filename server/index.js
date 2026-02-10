d35fcb3ff73eb52b602045db927963a831601306
uskokova1 <uskokova1@southernct.edu>
2026-02-10 16:31:06 -0500

    markdown test

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const dotenv = require('dotenv').config(); //initializes environment variables from .env file
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const PORT = 3000;
@@ -19,3 +21,47 @@ app.get('/api/test', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
+
+//code below is nowhere near done fr xd
+// Generating JWT
+app.post("/user/generateToken", (req, res) => {
+  // Validate User Here
+  // Then generate JWT Token
+
+  let jwtSecretKey = process.env.JWT_SECRET_KEY;
+  let data = {
+    time: Date(),
+    userId: 12,
+  }
+
+  const token = jwt.sign(data, jwtSecretKey);
+
+  res.send(token);
+});
+
+// Verification of JWT
+app.get("/user/validateToken", (req, res) => {
+  // Tokens are generally passed in header of request
+  // Due to security reasons.
+
+  let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+  let jwtSecretKey = process.env.JWT_SECRET_KEY;
+
+  try {
+    const token = req.header(tokenHeaderKey);
+
+    const verified = jwt.verify(token, jwtSecretKey);
+    if (verified) {
+      return res.send("Successfully Verified");
+    } else {
+      // Access Denied
+      return res.status(401).send(error);
+    }
+  } catch (error) {
+    // Access Denied
+    return res.status(401).send(error);
+  }
+});
+
