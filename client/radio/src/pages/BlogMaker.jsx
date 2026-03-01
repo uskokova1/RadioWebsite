7d94fa2c699a9a04285ef3677724802a2383c883
uskokova1 <uskokova1@southernct.edu>
2026-03-01 01:11:27 -0500

    blogeditor text boxes work

--- client/radio/src/pages/BlogMaker.jsx
+++ client/radio/src/pages/BlogMaker.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {Rnd} from "react-rnd";
 import MarkdownView from "react-showdown";
 import DOMPurify from "dompurify";
@@ -7,21 +7,76 @@ import {motion} from 'motion/react'
 const BlogMaker = () => {
 
     const [inputText, setInputText] = useState("");
+    const [currIndex, setCurrIndex] = useState(0);
+
+    const [textBoxes, setTextBoxes] = useState([]);
     const handleChange = (e) => {
-        setInputText(DOMPurify.sanitize(e.target.value));
+        const sanitized = DOMPurify.sanitize(e.target.value);
+
+        setInputText(sanitized);
+
+        setTextBoxes(prev =>
+            prev.map((box, i) =>
+                i === currIndex
+                    ? { ...box, text: sanitized }
+                    : box
+            )
+        );
     }
-    const [state, setState] = useState({
-        width: 200,
-        height: 200,
-        x: 100,
-        y: 100,
-    });
+    const addTextBox = (e) => {
+        setTextBoxes([...textBoxes, {
+            x: 200,
+            y:200,
+            width: 200,
+            height: 200,
+            text: 'edit text'
+        }]);
+    }
+    const clickTextBox = (e, index) => {
+        setCurrIndex(index);
+        setInputText(textBoxes[index].text)
+    }
+
 
     return (
-        <div>
-        <Rnd
+    <div className='flex'>
+        <div className='polka justify-self-center m-5 rounded-2xl w-full max-w-[760px] min-h-screen flex-col gap-0 shadow-md'>
+
+            {textBoxes.map((box, index) => (
+                <Rnd
+                    bounds="parent"
+                    key={index}
+                    position={{ x: box.x, y: box.y }}
+                    size={{ width: box.width, height: box.height }}
+                    onDragStop={(e, d) => {
+                        const updated = [...textBoxes];
+                        updated[index].x = d.x;
+                        updated[index].y = d.y;
+                        setTextBoxes(updated);
+                    }}
+                    onResizeStop={(e, dir, ref, delta, pos) => {
+                        const updated = [...textBoxes];
+                        updated[index].x = pos.x;
+                        updated[index].y = pos.y;
+                        updated[index].width = ref.offsetWidth;
+                        updated[index].height = ref.offsetHeight;
+                        setTextBoxes(updated);
+                    }}
+                    onClick={(e) => clickTextBox(e, index)}
+                >
+                    <div className='group hover:outline-4 hover:outline-dashed
+            outline-gray-300 p-2 overflow-clip bg-gray-200 h-full w-auto rounded-s'
+                    >
+                    <MarkdownView options={{ simpleLineBreaks: true }}
+                                  className='prose' markdown={textBoxes[index].text} />
+                    </div>
+                </Rnd>
+            ))}
+
+            {/*
+            <Rnd
             size={{ width: state.width, height: state.height }}
-            bounds="window"
+            bounds="parent"
             position={{ x: state.x, y: state.y }}
             onDragStop={(e, d) => {
                 setState({ ...state, x: d.x, y: d.y });
@@ -35,10 +90,7 @@ const BlogMaker = () => {
             }}
         >
 
-            <div className='group border-2 border-gray-300 p-2 overflow bg-gray-200 h-full w-auto rounded-s'>
-                <MarkdownView className='prose' markdown={inputText} />
 
-                {/*
                 <button className='opacity-0 group-hover:opacity-100
                 absolute bottom-0 right-0
                 rounded-full bg-gray-100 px-1
@@ -49,7 +101,7 @@ const BlogMaker = () => {
                 group-hover:translate-y-0
                 '>
                     Edit </button>
-                */}
+
                 <motion.button className='hidden group-hover:block
                                absolute bottom-0 left-2
                                rounded-full bg-gray-100 px-1 translate-y-1'
@@ -58,27 +110,37 @@ const BlogMaker = () => {
                                transition={{
                                 scale: {
                                    type: "spring",
-                                   duration: 2,
+                                   duration: 3,
                                    bounce: 0.8,
                                },
                                    y: {
                                        type: "spring",
-                                       duration: 1,
-                                       bounce: 0.8,
+                                       duration: 0.7,
+                                       bounce: 0.6,
                                    }
                                 }}
                 >
                     Edit
                 </motion.button>
-            </div>
         </Rnd>
-            <div className='absolute right align-middle'>
-                <textarea type="text" value={inputText} onChange={handleChange}/>
-            </div>
-            {/*
-            <button className='absolute p-5 m-5 bottom-0 spring-bounce-60 spring-duration-300 hover:scale-120 transition-transform rounded-full bg-gray-400'> jgiojfiew </button>
-            */}
+        */}
+
         </div>
+        <div className='flex-col p-10 w-max justify-center align-center'>
+            <textarea rows={17} className='flex bg-gray-100 rounded-3xl p-5 field-sizing-content'
+                type="text" value={inputText} onChange={handleChange}/>
+            <button
+                className='flex p-5 m-5 bottom-0 spring-bounce-60 spring-duration-300 hover:scale-110 transition-transform rounded-full bg-gray-200'
+                onClick={addTextBox}
+            >
+                add text box </button>
+            <button
+                className='flex p-5 m-5 bottom-0 spring-bounce-60 spring-duration-300 hover:scale-110 transition-transform rounded-full bg-gray-200'
+                onClick={addTextBox}
+            >
+                add img </button>
+        </div>
+    </div>
     )
 }
 export default BlogMaker
