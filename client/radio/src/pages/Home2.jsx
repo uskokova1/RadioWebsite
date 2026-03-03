ffeb89500141f19d10c3b2fe40022ec4d80550a2
uskokova1 <uskokova1@southernct.edu>
2026-03-03 18:18:34 -0500

    adding spring

--- client/radio/src/pages/Home2.jsx
+++ client/radio/src/pages/Home2.jsx
@@ -6,8 +6,7 @@ import * as rasterizeHTML from 'rasterizehtml';
 const Home2 = () => {
 
     return (
-    <div className='w-screen h-screen bg-white'>
-        {/*
+    <div className='absolute w-screen h-screen bg-black'>
         <PixelBlast
             variant="circle"
             pixelSize={2}
@@ -25,23 +24,34 @@ const Home2 = () => {
             liquidWobbleSpeed={5}
             speed={0.5}
             edgeFade={0.4}
-            transparent={false}
-        /> */}
+            transparent={true}
+        />
+        <div className='absolute left-1/2 top-0 w-150 h-100 -translate-x-1/2 -scale-y-100'>
+            <PixelBlast
+                variant="circle"
+                pixelSize={4}
+                color="#ff0000"
+                patternScale={2}
+                patternDensity={4}
+                pixelSizeJitter={0.85}
+                enableRipples={false}
+                rippleSpeed={0.4}
+                rippleThickness={0.12}
+                rippleIntensityScale={1.5}
+                liquid={true}
+                liquidStrength={0.02}
+                liquidRadius={0.2}
+                liquidWobbleSpeed={2}
+                speed={0.5}
+                edgeFade={1.2}
+                transparent={true}
+            />
+        </div>
         <Wigglie className="absolute top-10 left-1/2 -translate-x-1/2">
-            <svg viewBox='0 0 0 0' xmlns='http://www.w3.org/2000/svg'>
-                <defs>
-                <filter id='noiseFilter'>
-                    <feTurbulence
-                        type='fractalNoise'
-                        baseFrequency='0.65'
-                        numOctaves='3'
-                        stitchTiles='stitch'/>
-                </filter>
-                </defs>
-            </svg>
 
-            <h1 className='text-8xl text-[#ff0000] dissolve drop-shadow-gray-500 drop-shadow-2xl'
-                style={{ filter: 'url(#)' }}
+            <h1 className='relative z-10 text-8xl text-[#ff0000] hover:scale-110 transition-all'
+            > WSIN </h1>
+            <h1 className='absolute top-0 text-8xl text-[#ff0000] blur-md'
             > WSIN </h1>
 
             {/* <h1 className='text-8xl text-[#ff0000] dissolve drop-shadow-gray-500 drop-shadow-2xl'> WSIN </h1>
