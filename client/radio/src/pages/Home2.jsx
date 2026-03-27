e4deabfc074ef235c645b73108e967af7d3d9da8
munkE-dev <angeluskokov@gmail.com>
2026-03-27 19:11:09 -0400

    okay its workable now

--- client/radio/src/pages/Home2.jsx
+++ client/radio/src/pages/Home2.jsx
@@ -8,7 +8,9 @@ const App = () => {
     return (
         <div>
             <h1>Draggable & Scalable Containers</h1>
-            <DraggableWindow icon={<House />}>
+            <DraggableWindow icon={
+                <House className='absolute left-5 top-15 hover:scale-110 transition-all spring-duration-300 spring-bounce-60'/>
+            }>
                 <Login windowName='Login' spawnx={50} spawny={90}/>
                 <EmailVerify windowName='Email Verify' spawnx={350} spawny={290} />
                 <div className='text-4xl text-black top-5  w-full h-auto' spawnx={150} spawny={590}>Child 1 Content</div>
